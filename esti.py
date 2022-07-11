ff7a5931dbfcc7aed8b720e1622a99ac4bd74682
Yosshu <100824513+Yosshu@users.noreply.github.com>
2022-07-11 20:34:24 +0900

    1カメの画像をクリックしたら2カメの画像に線を引く
    結果が何かおかしい

--- esti.py
+++ esti.py
@@ -1,10 +1,13 @@
+from re import X
 import numpy as np
 import cv2
 import glob
 import pyautogui
 
+# 検出するチェッカーボードの交点の数
 tate = 7
 yoko = 10
+
 
 # 3次元座標を描画
 def draw(img, corners, imgpts):
@@ -16,8 +19,57 @@ def draw(img, corners, imgpts):
 
 # 画像のどこをクリックしたか返す
 def onMouse(event, x, y, flags, params):
+    global mtx, mtx2, tvecs, tvecs2, R, R2, img_axes2
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, y)
+        obj_i1x = x                                                         # 対象物の1カメ画像座標　クリックした点
+        obj_i1y = y
+        obj_n1x = (obj_i1x - mtx[0][2]) / mtx[0][0]                         # 対象物の1カメ正規化座標　原点を真ん中にしてから，焦点距離で割る
+        obj_n1y = (obj_i1y - mtx[1][2]) / mtx[1][1]
+        obj_n1 = [[obj_n1x], [obj_n1y], [1]]                                # 対象物の1カメ正規化画像座標系を1カメカメラ座標系に変換　(x_normimg, y_normimg, 1)
+        obj_w = (np.linalg.inv(R)) @ (np.array(obj_n1) - np.array(tvecs))      # ncoordを世界座標系に変換              Ｗ = Ｒ^T (Ｃ1 - ｔ)
+
+        obj_c2 = np.array(R2) @ np.array(obj_w) + np.array(tvecs2)          # wcoordを2カメのカメラ座標系に変換     Ｃ2 = ＲＷ + ｔ
+        obj_n2x = obj_c2[0][0][0]/obj_c2[0][2][0]                           # ccoord2を2カメの正規化画像座標系に変換　(Xc/Zc, Yc/Zc, 1)
+        obj_n2y = obj_c2[0][1][0]/obj_c2[0][2][0]
+        obj_i2x = obj_n2x * mtx2[0][0] + mtx2[0][2]                         # 正規化画像座標を2カメの画像座標系に変換　焦点距離を掛けてから，原点を左上にする
+        obj_i2y = obj_n2y * mtx2[1][1] + mtx2[1][2]
+        #print(rvecs)
+        #print(R)
+
+
+        camera1_w = (np.linalg.inv(R)) @ (np.array([[0], [0], [0]]) - np.array(tvecs))     # 1カメのワールド座標        Ｗ = Ｒ^T (Ｃ1 - ｔ)
+        camera1_c2 = np.array(R2) @ camera1_w + np.array(tvecs)                         # 2カメを原点としたカメラ座標系での1カメの位置
+        camera1_n2 = camera1_c2 / camera1_c2[0][2][0]                                   # 2カメの正規化画像座標系での1カメの位置
+        camera1_i2x = camera1_n2[0][0][0] * mtx2[0][0] + mtx2[0][2]                     # 2カメの画像座標系での1カメの位置
+        camera1_i2y = camera1_n2[0][1][0] * mtx2[1][1] + mtx2[1][2]
+
+        img_line = img_axes2.copy()
+
+        slope = (camera1_i2y - obj_i2y)/(camera1_i2x - obj_i2x)
+
+        #if camera1_c2 - obj_i2y > camera1_i2x - obj_i2x:
+
+
+        a = 0
+        dx = -1 * camera1_i2x - a
+        dy = slope * dx
+
+        #koko
+        dx2 = -1 * camera1_i2x + 640 + a
+        dy2 = slope * dx2
+        #kookmade
+
+        img_line = cv2.line(img_line, (int(camera1_i2x + dx), int(camera1_i2y + dy)), (int(camera1_i2x + dx2), int(camera1_i2y + dy2)), (0,255,255), 5)
+        #img_line = cv2.line(img_line, (int(camera1_i2x + dx), int(camera1_i2y + dy)), (int(obj_i2x - dx), int(obj_i2y - dy)), (0,255,255), 5)
+        cv2.imshow('Axes2',img_line)
+        #print(int(obj_i2x), int(obj_i2y))
+
+        print((int(camera1_i2x + dx), int(camera1_i2y + dy)), (int(camera1_i2x + dx2), int(camera1_i2y + dy2)))
+        
+
+
+
+
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -65,6 +117,14 @@ if ret and ret2 == True:
     cv2.waitKey(500)
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
     ret2, mtx2, dist2, rvecs2, tvecs2 = cv2.calibrateCamera(objpoints, imgpoints2, gray2.shape[::-1],None,None)
+
+
+
+
+
+    # 回転行列を3×1から3×3に変換
+    R, Jacob = cv2.Rodrigues(np.array(rvecs))
+    R2, Jacob2 = cv2.Rodrigues(np.array(rvecs2))
     """
     ret：
     mtx：camera matrix，カメラ行列(内部パラメータ)
@@ -89,8 +149,10 @@ if ret and ret2 == True:
     img_axes2 = draw(img_axes2,corners22,imgpts2)
     # img_axes = cv2.drawFrameAxes(img_axes, mtx, dist, rvecs[-1], tvecs[-1], 3, 3)
     cv2.imshow('Axes2',img_axes2)
-
+    
+    
     cv2.setMouseCallback('Axes', onMouse)
+
 
 
 #繰り返し分から抜けるためのif文
@@ -108,4 +170,7 @@ https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
 姿勢推定
 http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_calib3d/py_pose/py_pose.html#pose-estimation
 https://docs.opencv.org/4.x/d7/d53/tutorial_py_pose.html
+
+画像座標系からカメラ座標系への変換
+https://mem-archive.com/2018/10/13/post-682/
 """
