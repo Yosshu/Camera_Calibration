fa8ca2c3f57c3425ed020257b4297f22949a101a
Yosshu <100824513+Yosshu@users.noreply.github.com>
2022-11-21 16:46:05 +0900

    受け取ったInt16型の値によって条件分岐
    toggle_msg.data == 0　で停止
    toggle_msg.data == 1　で右回り
    toggle_msg.data == 2　で左回り
    
    今まで別のリポジトリにアップロードしていたが，これからはこのCamera_Calibrationというリポジトリにアップロードしていく．

---
+++ TurtleBot/velocity_mode.ino
@@ -0,0 +1,120 @@
+#include <ros.h>
+#include <std_msgs/Int16.h>
+
+ros::NodeHandle nh;
+
+#include <Dynamixel2Arduino.h>
+
+// Please modify it to suit your hardware.
+#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560) // When using DynamixelShield
+  #include <SoftwareSerial.h>
+  SoftwareSerial soft_serial(7, 8); // DYNAMIXELShield UART RX/TX
+  #define DXL_SERIAL   Serial
+  #define DEBUG_SERIAL soft_serial
+  const int DXL_DIR_PIN = 2; // DYNAMIXEL Shield DIR PIN
+#elif defined(ARDUINO_SAM_DUE) // When using DynamixelShield
+  #define DXL_SERIAL   Serial
+  #define DEBUG_SERIAL SerialUSB
+  const int DXL_DIR_PIN = 2; // DYNAMIXEL Shield DIR PIN
+#elif defined(ARDUINO_SAM_ZERO) // When using DynamixelShield
+  #define DXL_SERIAL   Serial1
+  #define DEBUG_SERIAL SerialUSB
+  const int DXL_DIR_PIN = 2; // DYNAMIXEL Shield DIR PIN
+#elif defined(ARDUINO_OpenCM904) // When using official ROBOTIS board with DXL circuit.
+  #define DXL_SERIAL   Serial3 //OpenCM9.04 EXP Board's DXL port Serial. (Serial1 for the DXL port on the OpenCM 9.04 board)
+  #define DEBUG_SERIAL Serial
+  const int DXL_DIR_PIN = 22; //OpenCM9.04 EXP Board's DIR PIN. (28 for the DXL port on the OpenCM 9.04 board)
+#elif defined(ARDUINO_OpenCR) // When using official ROBOTIS board with DXL circuit.
+  // For OpenCR, there is a DXL Power Enable pin, so you must initialize and control it.
+  // Reference link : https://github.com/ROBOTIS-GIT/OpenCR/blob/master/arduino/opencr_arduino/opencr/libraries/DynamixelSDK/src/dynamixel_sdk/port_handler_arduino.cpp#L78
+  #define DXL_SERIAL   Serial3
+  #define DEBUG_SERIAL Serial
+  const int DXL_DIR_PIN = 84; // OpenCR Board's DIR PIN.
+#elif defined(ARDUINO_OpenRB)  // When using OpenRB-150
+  //OpenRB does not require the DIR control pin.
+  #define DXL_SERIAL Serial1
+  #define DEBUG_SERIAL Serial
+  const int DXL_DIR_PIN = -1;
+#else // Other boards when using DynamixelShield
+  #define DXL_SERIAL   Serial1
+  #define DEBUG_SERIAL Serial
+  const int DXL_DIR_PIN = 2; // DYNAMIXEL Shield DIR PIN
+#endif
+ 
+
+const uint8_t DXL_ID = 1;
+const uint8_t DXL_ID2 = 2;
+const float DXL_PROTOCOL_VERSION = 2.0;
+
+
+Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+
+//This namespace is required to use Control table item names
+using namespace ControlTableItem;
+
+
+
+void messageCb( const std_msgs::Int16& toggle_msg) {
+  int DXL_VELOCITY = 0;
+  // put your main code here, to run repeatedly:
+  
+  // Please refer to e-Manual(http://emanual.robotis.com) for available range of value. 
+  // Set Goal Velocity using RAW unit
+  
+  if (toggle_msg.data == 0) {
+    DXL_VELOCITY = 0;
+  }
+  else if (toggle_msg.data == 1) {
+    DXL_VELOCITY = 100;
+  }
+  else if (toggle_msg.data == 2) {
+    DXL_VELOCITY = -100;
+  }
+  
+  
+  dxl.setGoalVelocity(DXL_ID, DXL_VELOCITY);
+  dxl.setGoalVelocity(DXL_ID2, DXL_VELOCITY);
+  // Print present velocity
+  //DEBUG_SERIAL.print("Present Velocity(raw) : ");
+  //DEBUG_SERIAL.println(dxl.getPresentVelocity(DXL_ID));
+
+
+}
+
+ros::Subscriber<std_msgs::Int16> sub("toggle_wheel", &messageCb );
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  // Use UART port of DYNAMIXEL Shield to debug.
+  DEBUG_SERIAL.begin(115200);
+  
+  // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
+  dxl.begin(1000000);
+  // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+  dxl.ping(DXL_ID);
+
+  // Turn off torque when configuring items in EEPROM area
+  dxl.torqueOff(DXL_ID);
+  dxl.setOperatingMode(DXL_ID, OP_VELOCITY);
+  dxl.torqueOn(DXL_ID);
+
+  dxl.ping(DXL_ID2);
+
+  // Turn off torque when configuring items in EEPROM area
+  dxl.torqueOff(DXL_ID2);
+  dxl.setOperatingMode(DXL_ID2, OP_VELOCITY);
+  dxl.torqueOn(DXL_ID2);
+
+  
+  nh.initNode();
+  nh.subscribe(sub);
+}
+
+
+void loop(){
+  nh.spinOnce();
+  delay(1);
+}
