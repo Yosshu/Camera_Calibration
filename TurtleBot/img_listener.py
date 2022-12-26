e661926c082fecfa4f5995a7dcaf642d1653a78c
Yosshu <100824513+Yosshu@users.noreply.github.com>
2022-12-26 18:13:53 +0900

    Add files via upload

--- TurtleBot/img_listener.py
+++ TurtleBot/img_listener.py
@@ -1,45 +1,45 @@
-#! /usr/bin/env python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Int16MultiArray
+from std_msgs.msg import Int32MultiArray
 import numpy as np
 import cv2
 
-import time
-
-class testNode():
-    def __init__(self):
-        # Subscriberの作成
-        self.sub = rospy.Subscriber('img_data', Int16MultiArray, self.callback)
-        # Publisherの作成
-        self.pub = rospy.Publisher('click_data', Int16MultiArray, queue_size=1)
-
-    def callback(self, data):
-        # callback関数の処理をかく
-        lis_array = np.array(data.data)
-        lis_array = lis_array.astype(np.uint8)
-        #送る画像サイズで調節(例：lis_array = lis_array.reshape([1080,1920,3]))
-        lis_array = lis_array.reshape([480,640,3])
-        cv2.imshow('lis_img',lis_array)
-        cv2.setMouseCallback('lis_img',self.onMouse)
-        cv2.waitKey(1)
 
 
-    def publish(self, x,y):
-        data_array = [x,y]
-        pub_array = Int16MultiArray(data=data_array)
-        self.pub.publish(pub_array)
+def onMouse(event, x, y, flags, params):      # 1カメの画像に対するクリックイベント
+    #global lis_array2
+    if event == cv2.EVENT_LBUTTONDOWN:      # 左クリック
+        z = params[y][x]
+        if z == 0:
+            print("人参しりしり")
+        else:
+            print(f'座標({x},{y})までの距離は{z/1000}[m]です！！！！！')
+        
 
-    def onMouse(self, event, x, y, flags, params):      # 1カメの画像に対するクリックイベント
-        if event == cv2.EVENT_LBUTTONDOWN:      # 左クリック
-            self.publish(x,y)
+def callback(data):
+    #global lis_array2
+    lis_array = np.array(data.data)
+    #print(lis_array[0:5])
+    
+    lis_array = lis_array.astype(np.uint16)
+    #送る画像サイズで調節(例：lis_array = lis_array.reshape([1080,1920,3]))
+    lis_array1 = lis_array[0:480*640*3]
+    lis_array2 = lis_array[921600:]
+    lis_array1 = lis_array1.astype(np.uint8)
+    lis_array1 = lis_array1.reshape([480,640,3])
+    lis_array2 = lis_array2.reshape([480,640])
+    cv2.imshow('color_image',lis_array1)
+    #cv2.imshow('depth_colormap',lis_array2)
+    cv2.setMouseCallback('color_image',onMouse, lis_array2)
+    cv2.waitKey(1)
+    
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('img_data', Int32MultiArray, callback)
+    rospy.spin()
+
 
 if __name__ == '__main__':
-    rospy.init_node('test_node2')
-
-    time.sleep(3.0)
-    node = testNode()
-
-    while not rospy.is_shutdown():
-        rospy.sleep(0.1)
+    listener()
