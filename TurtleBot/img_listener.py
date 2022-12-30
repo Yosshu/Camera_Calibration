79d1f024ae647a3332f255f4724e5bde4d47994d
Yosshu <100824513+Yosshu@users.noreply.github.com>
2022-12-30 16:19:54 +0900

    depth情報を送る
    クリックすることで取得したdepth情報をesti_ros.pyの方へROS通信で送る．
    型はInt16とする．10で割って，小数第一位は切り捨てる．

--- TurtleBot/img_listener.py
+++ TurtleBot/img_listener.py
@@ -2,45 +2,60 @@
 
 import rospy
 from std_msgs.msg import Int16MultiArray
+from std_msgs.msg import Int16
 import numpy as np
 import cv2
 
 
+class depthimg():
+    def __init__(self):
+        # Subscriberの作成
+        self.sub = rospy.Subscriber('img_data', Int16MultiArray, self.callback)
+        # Publisherの作成
+        self.pub = rospy.Publisher('depth_data', Int16, queue_size=1)
 
-def onMouse(event, x, y, flags, params):      # 1カメの画像に対するクリックイベント
-    #global lis_array2
-    if event == cv2.EVENT_LBUTTONDOWN:      # 左クリック
-        z = params[y][x]
-        if z == 0:
-            print("orz")
-        else:
-            print(f'座標({x},{y})までの距離は{z/1000}[m]です!!!!!!!')
+    def callback(self,data):
+        tate = 240
+        yoko = 320
+        lis_array = np.array(data.data)
 
+        lis_array = lis_array.astype(np.uint8)
+        #送る画像サイズで調節(例：lis_array = lis_array.reshape([1080,1920,3]))
+        lis_array1 = lis_array[0:tate*yoko*3]
+        lis_array21 = lis_array[tate*yoko*3:tate*yoko*4]
+        lis_array21 = lis_array21*256
+        lis_array22 = lis_array[tate*yoko*4:]
+        lis_array2 = lis_array21+lis_array22
+        lis_array1 = lis_array1.reshape([tate,yoko,3])
+        lis_array2 = lis_array2.reshape([tate,yoko])
+        lis_array1 = cv2.resize(lis_array1,dsize=(640,480))
+        lis_array2 = cv2.resize(lis_array2,dsize=(640,480))
+        cv2.imshow('color_image',lis_array1)
+        #lis_array2 = cv2.applyColorMap(cv2.convertScaleAbs(lis_array2, alpha=0.03),cv2.COLORMAP_JET)
+        #cv2.imshow('depth_colormap',lis_array2)
+        cv2.setMouseCallback('color_image',self.onMouse_lis, lis_array2)
+        cv2.waitKey(1)
 
-def callback(data):
-    lis_array = np.array(data.data)
+    def publish(self, data):
+        self.pub.publish(data)
 
-    lis_array = lis_array.astype(np.uint8)
-    #送る画像サイズで調節(例：lis_array = lis_array.reshape([1080,1920,3]))
-    lis_array1 = lis_array[0:921600]
-    lis_array21 = lis_array[921600:1228800]
-    lis_array21 = lis_array21*256
-    lis_array22 = lis_array[1228800:]
-    lis_array2 = lis_array21+lis_array22
-    lis_array1 = lis_array1.reshape([480,640,3])
-    lis_array2 = lis_array2.reshape([480,640])
-    cv2.imshow('color_image',lis_array1)
-    #lis_array2 = cv2.applyColorMap(cv2.convertScaleAbs(lis_array2, alpha=0.03),cv2.COLORMAP_JET)
-    #cv2.imshow('depth_colormap',lis_array2)
-    cv2.setMouseCallback('color_image',onMouse, lis_array2)
-    cv2.waitKey(1)
-    
+    def onMouse_lis(self, event, x, y, flags, params):      # 1カメの画像に対するクリックイベント
+        if event == cv2.EVENT_LBUTTONDOWN:      # 左クリック
+            z = params[y][x]
+            if z == 0:
+                print("Error")
+            else:
+                print(f'座標({x},{y})までの距離は{z/1000}[m]です')
+                self.publish(z//10)
 
-def listener():
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('img_data', Int16MultiArray, callback)
-    rospy.spin()
 
 
 if __name__ == '__main__':
-    listener()
+    rospy.init_node('listener')
+
+    #time.sleep(3.0)
+    dep = depthimg()
+
+    while not rospy.is_shutdown():
+        rospy.sleep(0.1)
+
